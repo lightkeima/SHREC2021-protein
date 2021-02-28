adc23a7ab84f6d0da5173f71c5bd381f320fb45d
Vinh-Thuyen <ntvthuyen@gmail.com>
2021-02-28 09:10:03 +0700

    update

--- models/cgcnn.py
+++ models/cgcnn.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from torch.nn import Linear, LayerNorm, ReLU
 from torch_scatter import scatter
 
-from torch_geometric.nn import GENConv, DeepGCNLayer
+from torch_geometric.nn import GENConv, DeepGCNLayer, CGConv, global_max_pool, global_mean_pool, global_add_pool
 from torch_geometric.data import RandomNodeSampler
 class CGCNN(torch.nn.Module):
   def __init__(self, args):
@@ -14,33 +14,15 @@ class CGCNN(torch.nn.Module):
     self.args = args
     self.num_features = args.num_features  
     self.num_classes = args.num_classes
-    hidden_channels = 128
-    self.node_encoder = Linear(4, hidden_channels)
-    #self.edge_encoder = Linear(3, hidden_channels)
-    num_layers = 12
-    self.layers = torch.nn.ModuleList()
-    for i in range(1, num_layers + 1):
-      conv = GENConv(hidden_channels, hidden_channels, aggr='softmax',
-                           t=1.0, learn_t=True, num_layers=2, norm='layer')
-      norm = LayerNorm(hidden_channels, elementwise_affine=True)
-      act = ReLU(inplace=True)
-
-      layer = DeepGCNLayer(conv, norm, act, block='res+', dropout=0.1,
-                                 ckpt_grad=i % 3)
-      self.layers.append(layer)
-
-    self.lin = Linear(hidden_channels, args.num_classes)
+    self.conv1 = CGConv(3,dim=4)
+    self.conv2 = CGConv(3,dim=4)
+    self.lin = Linear(3,64)
+    self.classifier = Linear(64, args.num_classes)
      
   def forward(self, data):
     x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
-    #edge_attr = self.edge_encoder(edge_attr)
-    x = self.node_encoder(x)
-    x = self.layers[0].conv(x, edge_index, None)
-
-    for layer in self.layers[1:]:
-      x = layer(x, edge_index, None)
-
-      x = self.layers[0].act(self.layers[0].norm(x))
-      x = F.dropout(x, p=0.1, training=self.training)
-
-    return self.lin(x)
+    x = self.conv1(x,edge_index,edge_attr)
+    x = self.conv2(x,edge_index,edge_attr)
+    x = self.lin(x)
+    x = global_max_pool(x,batch)
+    return self.classifier(x)
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 main.py --angle-and-distance --line-graph --face-to-edge 0 --meshes-to-points 0 --model cgcnn --lr 0.0005 --num-instances 10 --num-sample-points 512
+python3 main.py --angle-and-distance --face-to-edge 0 --meshes-to-points 0 --model genconv --lr 0.01 --num-instances 10 --num-sample-points 512
 #python3 main.py --face-to-edge 0 --meshes-to-points 1 --model edge_conv --layer dynamic_edge_conv --set-x 0 --lr 0.0005 --num-instances 10 --num-sample-points 512
 # python3 main.py
 # --face-to-edge 0 --meshes-to-points 1 --model edge_conv --layer dynamic_edge_conv --set-x 0 --lr 0.0005 --num-instances 5 --num-sample-points 32 --in-memory-dataset
